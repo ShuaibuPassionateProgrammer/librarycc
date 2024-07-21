3b98fe852c0f1be16a1924810c49af4a2d3898d0
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-07-21 07:19:11 +0100

    Initial commit

--- index.php
+++ index.php
@@ -1,20 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Library CC</title>
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <style>
-        .header {
-            background-color: #343a40;
-        }
-        .rounded {
-            border-radius: 20px !important;
-        }
-    </style>
-</head>
-<body>
+
     <div class="container-fluid header">
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
             <a href="index.php" class="navbar-brand">
