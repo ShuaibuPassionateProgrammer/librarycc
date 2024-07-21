49d215e5d20ce7720a18550f332a0e3935e9a678
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-07-21 07:08:59 +0100

    Initial commit

--- index.php
+++ index.php
@@ -20,6 +20,7 @@
             <a href="index.php" class="navbar-brand">
                 <img src="images/library_image.png" width="50" height="50" alt="Library Image Logo">
             </a>
+            <h2 class="text-light">Library Cat & Class</h2>
         </nav>
     </div>
 </body>
