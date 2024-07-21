8c2b3fab9df42e2aaa99fec0c01bf5a9d5f67732
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-07-21 07:58:09 +0100

    Initial commit

--- member/index.php
+++ member/index.php
@@ -34,7 +34,7 @@ include "includes/header.php";
                 </div>
                 <div class="form-group text-center">
                     <p>Not yet a member?<a href="register.php">Register</a></p>
-                    <p><a href="/librarycc">Back to Homepage</a></p>
+                    <p><a href="<?= __FILE__; ?>">Back to Homepage</a></p>
                 </div>
             </form>
         </div>
