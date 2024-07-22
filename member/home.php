1e2ad59846c2a58f6dc4e60d48fc1f0cee445294
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-07-22 13:39:51 +0100

    Working on member

--- member/home.php
+++ member/home.php
@@ -70,7 +70,9 @@ include "../config/db_con.php";
     }
     ?>
     <div class="container-fluid p-2">
-        <div class="row justify-content-center"></div>
+        <div class="row justify-content-center">
+            <div class="col-md-8"></div>
+        </div>
     </div>
 </body>
 </html>
