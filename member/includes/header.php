76c084c88c33157b33f7d2acc2c7a28a231c8aa1
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-07-22 06:14:34 +0100

    Initial commit

--- member/includes/header.php
+++ member/includes/header.php
@@ -4,6 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Library CC</title>
+    <style>
+        .header {
+            background-color: #343a40;
+        }
+        .rounded {
+            border-radius: 20px !important;
+        }
+    </style>
 </head>
 <body>
     
