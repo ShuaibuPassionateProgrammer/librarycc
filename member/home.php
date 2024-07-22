7b2aa38c33137de95786c8f2f0b183556acf96a0
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-07-22 13:45:44 +0100

    Working on member

--- member/home.php
+++ member/home.php
@@ -103,6 +103,8 @@ include "../config/db_con.php";
         </div>
     </div>
 
-    <div class="container p-2"></div>
+    <div class="container p-2">
+        <div class="row justify-content-center"></div>
+    </div>
 </body>
 </html>
