dc07a9c8004c1918a3a9d42480b08ff088a140cd
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-07-22 13:45:55 +0100

    Working on member

--- member/home.php
+++ member/home.php
@@ -104,7 +104,9 @@ include "../config/db_con.php";
     </div>
 
     <div class="container p-2">
-        <div class="row justify-content-center"></div>
+        <div class="row justify-content-center">
+            <div class="col-md-12"></div>
+        </div>
     </div>
 </body>
 </html>
