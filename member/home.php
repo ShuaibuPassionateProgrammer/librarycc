9643d1a298f92466a6aeb6e131cc3c9fe80479d0
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-07-22 13:46:17 +0100

    Working on member

--- member/home.php
+++ member/home.php
@@ -105,7 +105,12 @@ include "../config/db_con.php";
 
     <div class="container p-2">
         <div class="row justify-content-center">
-            <div class="col-md-12"></div>
+            <div class="col-md-12">
+                <?php
+                if(isset($_POST['btn_search']))
+                {}
+                ?>
+            </div>
         </div>
     </div>
 </body>
