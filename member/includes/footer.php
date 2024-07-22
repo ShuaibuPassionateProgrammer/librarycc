f2de84505b9031c51684092f6e94379fa9087ce6
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-07-22 06:17:52 +0100

    Initial commit

--- member/includes/footer.php
+++ member/includes/footer.php
@@ -2,4 +2,10 @@
     <script src="js/jquery-3.3.1.slim.min.js"></script>
     <script src="js/popper.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
-    <script></script>
+    <script>
+        $(document).ready(function() {
+            setTimeout(function() {
+                $('.alert').hide();
+            }, 3000);
+        });
+    </script>
