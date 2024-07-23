a05159ef77dfd73d0f10714bf1c6fb41ad24ac6c
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-07-24 00:15:01 +0100

    Working on member

--- member/register.php
+++ member/register.php
@@ -14,7 +14,9 @@ include "includes/header.php";
 <div class="container p-2">
     <div class="row justify-content-center">
         <div class="col-md-12">
-            <div class="card"></div>
+            <div class="card">
+                <div class="card-header text-center"></div>
+            </div>
         </div>
     </div>
 </div>
