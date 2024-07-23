1127ad8cc070308322e94197afc26cef737bd10b
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-07-23 23:14:16 +0100

    Working on member

---
+++ member/includes/navbar.php
@@ -0,0 +1,8 @@
+    <div class="container-fluid header">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <a href="index.php" class="navbar-brand">
+                <img src="images/library_image.png" width="50" height="50" alt="Library Image Logo">
+            </a>
+            <h2 class="text-light">Library Cat & Class</h2>
+        </nav>
+    </div>
