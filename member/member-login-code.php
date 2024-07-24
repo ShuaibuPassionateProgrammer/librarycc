454318546a5fac6f7f855864b187df37d86a1f40
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-07-24 06:01:55 +0100

    Working on member

--- member/member-login-code.php
+++ member/member-login-code.php
@@ -6,4 +6,5 @@ if(isset($_POST['login']))
 {
     $username = mysqli_real_escape_string($conn, $_POST['username']);
     $password = mysqli_real_escape_string($conn, $_POST['password']);
+    $password = sha1($password);
 }
