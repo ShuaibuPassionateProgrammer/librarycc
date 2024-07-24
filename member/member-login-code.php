70e50ab4a4b6d848dde514f83e4847ec3252c5e8
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-07-24 06:02:09 +0100

    Working on member

--- member/member-login-code.php
+++ member/member-login-code.php
@@ -7,4 +7,6 @@ if(isset($_POST['login']))
     $username = mysqli_real_escape_string($conn, $_POST['username']);
     $password = mysqli_real_escape_string($conn, $_POST['password']);
     $password = sha1($password);
+
+    $query = "SELECT * FROM member WHERE username='$username' AND password='$password' LIMIT 1";
 }
