eadbc79b6587d28a3ca367271b183a72138f1b9e
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-07-24 01:00:01 +0100

    Working on member

--- member/member_dashboard.php
+++ member/member_dashboard.php
@@ -15,6 +15,8 @@
     </style>
 </head>
 <body>
-    <div class="container-fluid header"></div>
+    <div class="container-fluid header">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark"></nav>
+    </div>
 </body>
 </html>
