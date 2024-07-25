50a17bfb5a30739f5ed457e032c63105d677b84c
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-07-25 15:57:33 +0100

    Working on Librarian

--- librarian/message.php
+++ librarian/message.php
@@ -4,6 +4,7 @@ if(isset($_SESSION['response']))
     ?>
     <div class="alert alert-<?php echo $_SESSION['res_type']; ?> alert-dismissible fade show">
         <?php echo $_SESSION['response']; ?>
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close"></button>
     </div>
     <?php
 }
