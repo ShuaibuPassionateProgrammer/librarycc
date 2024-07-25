ab0200a39ae054603d0fc3491dc8c52f43dffbd7
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-07-25 16:29:00 +0100

    Working on member

--- member/register.php
+++ member/register.php
@@ -9,7 +9,7 @@ include "includes/header.php";
             <?php include("message.php"); ?>
         </div>
     </div>
-</div>
+</div> 
 
 <div class="container p-2">
     <div class="row justify-content-center">
@@ -29,46 +29,47 @@ include "includes/header.php";
             <form action="member-register-code.php" method="post">
                 <fieldset class="border p-2">
                     <legend class="float-none w-auto">Registering a member</legend>
-
                     <div class="row">
                         <div class="col-md-6">
                             <table class="table table-borderless">
                                 <tbody>
                                     <tr>
                                         <td>First Name</td>
-                                        <td><input type="text" name="firstname" class="form-control">
+                                        <td><input type="text" name="firstname" class="form-control"></td>
                                     </tr>
                                     <tr>
                                         <td>Last Name</td>
                                         <td><input type="text" name="lastname" class="form-control"></td>
                                     </tr>
                                     <tr>
+                                        <td>Username</td>
                                         <td><input type="text" name="username" class="form-control"></td>
                                     </tr>
                                 </tbody>
                             </table>
                         </div>
-
                         <div class="col-md-6">
-                            <table class="table-borderless">
-                                <tr>
-                                    <td>Email Address</td>
-                                    <td><input type="text" name="email" class="form-control"></td>
-                                </tr>
-                                <tr>
-                                    <td>Password</td>
-                                    <td><input type="password" name="password" class="form-control"></td>
-                                </tr>
-                                <tr>
-                                    <td>Confirm Password</td>
-                                    <td><input type="password" name="cpassword" class="form-control"></td>
-                                </tr>
-                                <tr>
-                                    <td colspan="2" class="text-right">
-                                        <a href="../member" class="btn btn-outline-secondary">Back</a>
-                                        <input type="submit" name="register" value="Register" class="btn btn-outline-primary">
-                                    </td>
-                                </tr>
+                            <table class="table table-borderless">
+                                <tbody>
+                                    <tr>
+                                        <td>Email Address</td>
+                                        <td><input type="text" name="email" class="form-control"></td>
+                                    </tr>
+                                    <tr>
+                                        <td>Password</td>
+                                        <td><input type="password" name="password" class="form-control"></td>
+                                    </tr>
+                                    <tr>
+                                        <td>Confirm Password</td>
+                                        <td><input type="password" name="cpassword" class="form-control"></td>
+                                    </tr>
+                                    <tr>
+                                        <td colspan="2" class="text-right">
+                                            <a href="../member" class="btn btn-outline-secondary">Back</a>
+                                            <input type="submit" name="register" value="Register" class="btn btn-outline-primary">
+                                        </td>
+                                    </tr>
+                                </tbody>
                             </table>
                         </div>
                     </div>
@@ -77,4 +78,5 @@ include "includes/header.php";
         </div>
     </div>
 </div>
+
 <?php include("includes/footer.php"); ?>
