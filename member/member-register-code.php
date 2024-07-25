eb2c36c9c2991f2eed148fe1c517ad6c0b1892eb
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-07-25 15:43:35 +0100

    Working on member

--- member/member-register-code.php
+++ member/member-register-code.php
@@ -6,4 +6,5 @@ if(isset($_POST['register']))
 {
     $firstname = mysqli_real_escape_string($conn, $_POST['firstname']);
     $lastname = mysqli_real_escape_string($conn, $_POST['lastname']);
+    $username = mysqli_real_escape_string($conn, $_POST['username']);
 }
