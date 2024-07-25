4274a2a62656e0df161f99b3aa6f2fb2f4f3bf7d
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-07-25 16:28:04 +0100

    Working on member

--- member/member_dashboard.php
+++ member/member_dashboard.php
@@ -1,8 +1,13 @@
+<?php //include("includes/header.php"); ?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Library CC</title>
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <style>
@@ -15,37 +20,49 @@
     </style>
 </head>
 <body>
+
     <div class="container-fluid header">
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
             <a href="index.php" class="navbar-brand">
                 <img src="images/library_image.png" width="50" height="50" alt="Library Image Logo">
             </a>
             <h3 class="text-light">Library CC</h3>
+            <?php
+                /*if(isset($_SESSION['auth']))
+                {
+                    echo $_SESSION['auth_librarian']['librarian_username'];
+                }
+                else
+                {
+                    echo "Not logged in";
+                }*/
+            ?>
             <div class="collapse navbar-collapse">
-                <div class="collapse navbar-collapse">
-                    <ul class="navbar-nav ml-auto">
-                        <li class="nav-item dropdown">
-                            <form action="logout.php" method="post">
-                                <button type="submit" name="logout_btn" class="btn btn-secondary">Logout</button>
-                            </form>
-                        </li>
-                    </ul>
-
-                    <div class="dropdown-menu">
-                        <a href="#" class="dropdown-item">Profile</a>
-                        <a href="#" class="dropdown-item">Settings</a>
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item dropdown">
                         <form action="logout.php" method="post">
-                            <button type="submit" name="logout_btn" class="dropdown-item">Logout</button>
+                            <button type="submit" name="logout_btn" class="btn btn-secondary">Logout</button>
                         </form>
-                    </div>
+                    </li>
+                </ul>
+                <div class="dropdown-menu">
+                    <a href="#" class="dropdown-item">Profile</a>
+                    <a href="#" class="dropdown-item">Settings</a>
+                    <form action="logout.php" method="post">
+                        <button type="submit" name="logout_btn" class="dropdown-item">Logout</button>
+                    </form>
                 </div>
             </div>
         </nav>
     </div>
 
-    <?php
+
+
+
+
+<?php
     session_start();
-    
+
     if(!isset($_SESSION['auth']))
     {
         $_SESSION['response'] = "Access denied! Login to access the member dashboard.";
@@ -53,58 +70,64 @@
         header("location: ../member");
         exit(0);
     }
-    else {
+    else
+    {
         if(isset($_SESSION['auth']['auth_member']))
         {
             echo "Hey! ".$_SESSION['auth_member']['member_username']." You are welcome to Librarian Dashboard";
             unset($_SESSION['auth_member']['member_username']);
         }
+        
     }
-    ?>
-    <div class="container p-2">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <?php inlcude("message.php"); ?>
-            </div>
-        </div>
-    </div>
+?>
 
-    <div class="container p-2">
-        <div class="row justify-content-center">
-            <div class="col-md-4">
-                <a href="add_books.php">
-                    <div class="card rounded">
-                        <div class="card-body text-center">
-                            <button class="btn btn-outline-primary rounded librarian w-50">Add Books</button>
-                        </div>
-                    </div>
-                </a>
-            </div>
-            <div class="col-md-4">
-                <a href="view_books.php">
-                    <div class="card rounded">
-                        <div class="card-body text-center">
-                            <button class="btn btn-outline-primary rounded librarian w-50">View Books</button>
-                        </div>
-                    </div>
-                </a>
-            </div>
-            <div class="col-md-4">
-                <a href="update_books.php">
-                    <div class="card-rounded">
-                        <div class="card-body text-center">
-                            <button class="btn btn-outline-primary rounded librarian w-50">Update Books</button>
-                        </div>
-                    </div>
-                </a>
-            </div>
-            <div class="col-md-4">
-                <a href="delete_books.php">
-                    <div class="card-rounded">
-                        <div class="card-body text-center"></div>
-                    </div>
-                </a>
-            </div>
+<div class="container p-2">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <?php include("message.php"); ?>
         </div>
     </div>
+</div>
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-4">
+            <a href="add_books.php">
+                <div class="card rounded">
+                    <div class="card-body text-center">
+                        <button class="btn btn-outline-primary rounded librarian w-50">Add Books</button>
+                    </div>
+                </div>
+            </a>
+        </div>
+        <div class="col-md-4">
+            <a href="view_books.php">
+                <div class="card rounded">
+                    <div class="card-body text-center">
+                        <button class="btn btn-outline-primary rounded librarian w-50">View Books</button>
+                    </div>
+                </div>
+            </a>
+        </div>
+        <div class="col-md-4">
+            <a href="update_books.php">
+                <div class="card rounded">
+                    <div class="card-body text-center">
+                        <button class="btn btn-outline-primary rounded librarian w-50">Update Books</button>
+                    </div>
+                </div>
+            </a>
+        </div>
+        <div class="col-md-4">
+            <a href="delete_books.php">
+                <div class="card rounded">
+                    <div class="card-body text-center">
+                        <button class="btn btn-outline-primary rounded librarian w-50">Delete Books</button>
+                    </div>
+                </div>
+            </a>
+        </div>
+    </div>
+</div>
+
 <?php include("includes/footer.php"); ?>
