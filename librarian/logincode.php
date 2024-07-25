ebbe00cfe65dcd1a8085c9b23bf25e4ba337182a
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-07-25 16:29:44 +0100

    Working on Librarian

--- librarian/logincode.php
+++ librarian/logincode.php
@@ -8,4 +8,6 @@ if(isset($_POST['login']))
     $password = mysqli_real_escape_string($conn, $_POST['password']);
 
     $password = sha1($password);
+
+    $query = "SELECT * FROM librarian WHERE username='$username' AND password='$password' LIMIT 1";
 }
