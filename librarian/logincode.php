e45f1db63ff4c2747e0d3086432eec7003da6731
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-07-25 16:02:33 +0100

    Working on Librarian

--- librarian/logincode.php
+++ librarian/logincode.php
@@ -5,4 +5,5 @@ include "../config/db_con.php";
 if(isset($_POST['login']))
 {
     $username = mysqli_real_escape_string($conn, $_POST['username']);
+    $password = mysqli_real_escape_string($conn, $_POST['password']);
 }
