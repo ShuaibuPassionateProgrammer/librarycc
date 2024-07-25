fdfd347eb1b6285520e02478a94e39103c42dbf7
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-07-25 16:29:13 +0100

    Working on member

--- member/includes/header.php
+++ member/includes/header.php
@@ -3,7 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Library CC</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
     <style>
         .header {
             background-color: #343a40;
@@ -15,3 +17,11 @@
 </head>
 <body>
 
+    <div class="container-fluid header">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <a href="index.php" class="navbar-brand">
+                <img src="images/library_image.png" width="50" height="50" alt="Library Image Logo">
+            </a>
+            <h2 class="text-light">Library Cat & Class</h2>
+        </nav>
+    </div>
