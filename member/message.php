6f31e1ea4068a714df92ef22a5fd70161b7db170
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-07-25 15:51:32 +0100

    Working on member

--- member/message.php
+++ member/message.php
@@ -4,7 +4,9 @@ if(isset($_SESSION['response']))
     ?>
     <div class="alert alert-<?php echo $_SESSION['res_type']; ?>
         <?php echo $_SESSION['response']; ?>
-        <button type="button" class="close" data-dismiss="alert" aria-label="Close"></button>
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
     </div>
     <?php
 }
