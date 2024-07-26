817d68aa7223beeddcd5a4a3920ff2c6d21cd186
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-07-26 16:47:22 +0100

    Working on Librarian

--- librarian/add_books.php
+++ librarian/add_books.php
@@ -41,5 +41,7 @@ else
 </div>
 
 <div class="container p-2">
-    <div class="row justify-content-center"></div>
+    <div class="row justify-content-center">
+        <div class="col-md-12"></div>
+    </div>
 </div>
