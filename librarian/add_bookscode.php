f2df339bdedaccf4c7c8819c9cc2c6bbde3e7e06
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-07-26 17:28:12 +0100

    Working on Librarian

--- librarian/add_bookscode.php
+++ librarian/add_bookscode.php
@@ -5,5 +5,6 @@ include "../config/db_con.php";
 if(isset($_POST['add_book']))
 {
     $main_author = mysqli_real_escape_string($conn, $_POST['main_author']);
+    $other_author = mysqli_real_escape_string($conn, $_POST['other_author']);
 }
 ?>
