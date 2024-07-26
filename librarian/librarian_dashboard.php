b96e3a871c7ca41b9eeda4bf0f6854d675a73b07
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-07-26 21:25:10 +0100

    Working on Librarian

--- librarian/librarian_dashboard.php
+++ librarian/librarian_dashboard.php
@@ -28,5 +28,7 @@ else {
 </div>
 
 <div class="container mt-5">
-    <div class="row justify-content-center mt-5"></div>
+    <div class="row justify-content-center mt-5">
+        <div class="col-md-4 mt-5"></div>
+    </div>
 </div>
