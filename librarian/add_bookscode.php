071edbcbd78ef4c781124fbb11622389771c41d1
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-07-26 21:15:10 +0100

    Working on Librarian

--- librarian/add_bookscode.php
+++ librarian/add_bookscode.php
@@ -52,6 +52,7 @@ if(isset($_POST['add_book']))
             else {
                 $_SESSION['response'] = "Failed to Save Book";
                 $_SESSION['res_type'] = "danger";
+                header("location: add_books.php");
             }
         }
     }
