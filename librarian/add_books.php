27bfded3a05cfe1e25838b2fa59a49ecf9cfa796
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-07-26 16:47:32 +0100

    Working on Librarian

--- librarian/add_books.php
+++ librarian/add_books.php
@@ -42,6 +42,8 @@ else
 
 <div class="container p-2">
     <div class="row justify-content-center">
-        <div class="col-md-12"></div>
+        <div class="col-md-12">
+            <form action="add_bookscode.php" method="post" enctype="multipart/form-data"></form>
+        </div>
     </div>
 </div>
