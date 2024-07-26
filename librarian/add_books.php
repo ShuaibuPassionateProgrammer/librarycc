d9350b4666a3191099c8ffb564b6f5b5b59bbd6f
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-07-26 16:43:59 +0100

    Working on Librarian

--- librarian/add_books.php
+++ librarian/add_books.php
@@ -31,7 +31,9 @@ else
 <div class="container p-2">
     <div class="row justify-content-center">
         <div class="col-md-12">
-            <div class="card"></div>
+            <div class="card">
+                <div class="card-body text-center">
+            </div>
         </div>
     </div>
 </div>
