63b15f4c1b13d44845c486ef434097706d254028
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-07-27 08:24:42 +0100

    Working on Librarian

--- librarian/view_books.php
+++ librarian/view_books.php
@@ -103,6 +103,7 @@ else {
                                         $query_run = mysqli_query($conn, $query);
 
                                         $sn = 1;
+                                        if(mysqli_num_rows($query_run) > 0) {}
                                         ?>
                                     </tbody>
                                 </table>
