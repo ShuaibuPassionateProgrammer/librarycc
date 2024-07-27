f155cefb9bce8640fc541aa3af2dac0727014b69
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-07-27 08:08:04 +0100

    Working on Librarian

--- librarian/view_books.php
+++ librarian/view_books.php
@@ -33,7 +33,9 @@ else {
     <div class="row justify-content-center">
         <div class="col-md-12">
             <div class="card">
-                <div class="card-header text-center"></div>
+                <div class="card-header text-center">
+                    <h3>Book Classifications</h3>
+                </div>
             </div>
         </div>
     </div>
