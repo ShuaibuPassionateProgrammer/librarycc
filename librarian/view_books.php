9a81e6912595cf5c08ced906875856da8b63ac4e
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-07-27 08:25:33 +0100

    Working on Librarian

--- librarian/view_books.php
+++ librarian/view_books.php
@@ -107,6 +107,7 @@ else {
                                             while($row = mysqli_fetch_assoc($query_run)) {
                                                 $main_author = $row['main_author'];
                                                 $other_author = $row['other_author'];
+                                                $editors = $row['editors'];
                                             }
                                         }
                                         ?>
