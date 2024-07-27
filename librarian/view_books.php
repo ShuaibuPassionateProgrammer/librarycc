91d35f8755e490ad20095973795d265d9a7c0926
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-07-27 08:43:34 +0100

    Working on Librarian

--- librarian/view_books.php
+++ librarian/view_books.php
@@ -189,7 +189,9 @@ else {
         <div class="col-md-12">
             <table class="table table-bordered table-responsive">
                 <thead>
-                    <tr></tr>
+                    <tr>
+                        <th colspan="22" class="text-center"><h3>Available Books</h3></th>
+                    </tr>
                 </thead>
             </table>
         </div>
