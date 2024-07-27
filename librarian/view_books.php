75473aac5f62171710362fc741d59aa97c94d109
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-07-27 09:16:27 +0100

    Working on Librarian

--- librarian/view_books.php
+++ librarian/view_books.php
@@ -234,6 +234,7 @@ else {
                             $title = $row['title'];
                             $edition = $row['edition'];
                             $tmaterial = $row['tmaterial'];
+                            $publication = $row['publication'];
                         }
                     }
                     ?>
