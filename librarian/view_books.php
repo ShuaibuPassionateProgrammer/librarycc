8c7d91618bbe1913f63766305e53255a2f403630
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-07-27 08:14:42 +0100

    Working on Librarian

--- librarian/view_books.php
+++ librarian/view_books.php
@@ -65,7 +65,9 @@ else {
                 {
                     ?>
                     <div class="container-fluid p-2">
-                        <div class="row justify-content-center"></div>
+                        <div class="row justify-content-center">
+                            <div class="col-md-12"></div>
+                        </div>
                     </div>
                     <?php
                 }
