64351eed710258c300056dff33a415c4f5ff9485
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-07-27 08:02:21 +0100

    Working on Librarian

--- librarian/view_books.php
+++ librarian/view_books.php
@@ -10,5 +10,6 @@ if(!isset($_SESSION['auth']))
     $_SESSION['response'] = "Access denied! Login to access the dashboard.";
     $_SESSION['res_type'] = "warning";
     header("location: ../librarian");
+    exit(0);
 }
 ?>
