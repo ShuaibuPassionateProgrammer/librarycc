4fc5289f9919aa732db853d66da3f9281cb986d7
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-07-27 09:40:04 +0100

    Working on Librarian

---
+++ librarian/includes/footer.php
@@ -0,0 +1,13 @@
+
+    <script src="js/jquery-3.3.1.slim.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            setTimeout(function() {
+                $('.alert').hide();
+            }, 3000);
+        });
+    </script>
+</body>
+</html>
