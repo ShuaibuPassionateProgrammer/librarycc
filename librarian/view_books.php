f03f6a96d11d00d4badeee4c3d7817fc406ca737
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-07-27 08:07:48 +0100

    Working on Librarian

--- librarian/view_books.php
+++ librarian/view_books.php
@@ -32,7 +32,9 @@ else {
 <div class="container-fluid p-2">
     <div class="row justify-content-center">
         <div class="col-md-12">
-            <div class="card"></div>
+            <div class="card">
+                <div class="card-header text-center"></div>
+            </div>
         </div>
     </div>
 </div>
