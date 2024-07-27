5ac6ec108cccc73b8a5ac29333788c1231a9ee40
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-07-27 08:08:54 +0100

    Working on Librarian

--- librarian/view_books.php
+++ librarian/view_books.php
@@ -36,7 +36,9 @@ else {
                 <div class="card-header text-center">
                     <h3>Book Classifications</h3>
                 </div>
-                <div class="card-body text-right"></div>
+                <div class="card-body text-right">
+                    <form method="post"></form>
+                </div>
             </div>
         </div>
     </div>
