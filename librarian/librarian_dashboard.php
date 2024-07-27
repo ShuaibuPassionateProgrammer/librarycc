fc5d31dfaa465fd17ff95e097f04f294cb8b27d6
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-07-27 09:37:00 +0100

    Working on Librarian

--- librarian/librarian_dashboard.php
+++ librarian/librarian_dashboard.php
@@ -1,22 +1,24 @@
 <?php include("includes/header.php"); ?>
 
 <?php
-session_start();
+    session_start();
 
-if(!isset($_SESSION['auth']))
-{
-    $_SESSION['response'] = "Access denied! Login to access the dashboard.";
-    $_SESSION['res_type'] = "warning";
-    header("location: ../librarian");
-    exit(0);
-}
-else {
-    if(isset($_SESSION['auth']['auth_librarian']))
+    if(!isset($_SESSION['auth']))
     {
-        echo "Hey! ".$_SESSION['auth_librarian']['librarian_username']." You are welcome to Librarian Dashboard";
-        unset($_SESSION['auth_librarian']['librarian_username']);
+        $_SESSION['response'] = "Access denied! Login to access the dashboard.";
+        $_SESSION['res_type'] = "warning";
+        header("location: ../librarian");
+        exit(0);
     }
-}
+    else
+    {
+        if(isset($_SESSION['auth']['auth_librarian']))
+        {
+            echo "Hey! ".$_SESSION['auth_librarian']['librarian_username']." You are welcome to Librarian Dashboard";
+            unset($_SESSION['auth_librarian']['librarian_username']);
+        }
+        
+    }
 ?>
 
 <div class="container p-2 mt-5">
@@ -32,7 +34,6 @@ else {
         <div class="col-md-4 mt-5">
             <a href="add_books.php" class="btn btn-outline-primary btn-lg rounded w-100">Cataloguing Worksheet</a>
         </div>
-
         <div class="col-md-4 mt-5">
             <a href="view_books.php" class="btn btn-outline-primary btn-lg rounded w-100">Classification Worksheet</a>
         </div>
