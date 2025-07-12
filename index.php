94b2e099cc42f5e17127fa3e7cd90747ee661d36
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-07-12 23:30:57 +0100

    Improving Home

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php include("includes/header.php"); ?>
 <div class="container d-flex align-items-center justify-content-center" style="min-height: 80vh;">
-    <div class="row g-5 justify-content-center"></div>
+    <div class="row g-5 justify-content-center">
+        <!-- Member Card -->
+    </div>
 </div>
