db9236849bfdc19ea229802cc2e15eca781bc6c0
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-07-12 23:32:28 +0100

    Improving Home

--- index.php
+++ index.php
@@ -5,7 +5,9 @@
         <div class="col-md-4 col-sm-6">
             <a href="member/" class="text-decoration-none">
                 <div class="card shadow-sm hover-shadow transition rounded text-center h-100">
-                    <div class="card-body"></div>
+                    <div class="card-body">
+                        <img src="images/img_4044.jpg" alt="Member" class="img-fluid mb-3 rounded-circle shadow" style="width: 120px; height: 120px; object-fit: cover;"></div>
+                    </div>
                 </div>
             </a>
         </div>
