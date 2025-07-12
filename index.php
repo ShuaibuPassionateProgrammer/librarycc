b2b92386a0a8b4f701ff2f05dcd90224f8ad378e
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-07-12 23:30:42 +0100

    Improving Home

--- index.php
+++ index.php
@@ -1,2 +1,4 @@
 <?php include("includes/header.php"); ?>
-<div class="container d-flex align-items-center justify-content-center" style="min-height: 80vh;"></div>
+<div class="container d-flex align-items-center justify-content-center" style="min-height: 80vh;">
+    <div class="row g-5 justify-content-center"></div>
+</div>
