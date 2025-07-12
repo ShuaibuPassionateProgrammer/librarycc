2ef24a4a84303ccfe3bd4f0a2bca94e6650c4de2
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-07-12 23:38:02 +0100

    Improving Home

--- index.php
+++ index.php
@@ -22,7 +22,10 @@
         <div class="col-md-4 col-sm-6">
             <a href="librarian/" class="text-decoration-none">
                 <div class="card shadow-sm hover-shadow transition rounded text-center h-100">
-                    <div class="card-body"></div>
+                    <div class="card-body">
+                        <img src="images/img_4045.png" alt="Librarian" class="img-fluid mb-3 rounded-circle shadow" style="width: 120px; height: 120px; object-fit: cover;">
+                        <h5 class="mt-3">Librarian</h5>
+                    </div>
                 </div>
             </a>
         </div>
