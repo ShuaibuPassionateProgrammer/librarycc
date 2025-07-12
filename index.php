8ce9a321945140a2493ce6e2751b1347f1792d80
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-07-12 23:31:18 +0100

    Improving Home

--- index.php
+++ index.php
@@ -2,5 +2,6 @@
 <div class="container d-flex align-items-center justify-content-center" style="min-height: 80vh;">
     <div class="row g-5 justify-content-center">
         <!-- Member Card -->
+        <div class="col-md-4 col-sm-6"></div>
     </div>
 </div>
