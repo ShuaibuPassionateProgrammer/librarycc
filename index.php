f6bf5cdbe7ecf336b7d93e8976103fbeda221a3a
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-07-12 23:29:07 +0100

    Working on Librarian

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php include("includes/header.php"); ?>
+<?php include("includes/navbar.php"); ?>
+
+    <div class="container mt-5">
+        <div class="row justify-content-center">
+            <div class="col-md-4">
+                <a href="member/">
+                    <div class="card rounded">
+                        <div class="card-body text-center">
+                            <img src="images/img_4044.jpg" alt="" class="logo p-2" style="box-shadow: 0 1px 5px rgba(2,2,2,.2); border-radius: 200px">
+                        </div>
+                        <div class="card-body text-center">
+                            <button class="btn btn-outline-primary rounded librarian w-50">Member</button>
+                        </div>
+                    </div>
+                </a>
+            </div>
+
+            <div class="col-md-4">
+                <a href="librarian/">
+                    <div class="card rounded">
+                        <div class="card-body text-center">
+                            <img src="images/img_4045.png" alt="" class="logo p-2" style="box-shadow: 0 1px 5px rgba(2,2,2,.2); border-radius: 200px">
+                        </div>
+                        <div class="card-body text-center">
+                            <button class="btn btn-outline-primary rounded librarian w-50">Librarian</button>
+                        </div>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+<?php include("includes/footer.php"); ?>
