1e576c6eec04deed7251603357c8b5c47c7e7bec
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-07-12 23:31:53 +0100

    Improving Home

--- index.php
+++ index.php
@@ -3,7 +3,9 @@
     <div class="row g-5 justify-content-center">
         <!-- Member Card -->
         <div class="col-md-4 col-sm-6">
-            <a href="member/" class="text-decoration-none"></a>
+            <a href="member/" class="text-decoration-none">
+                <div class="card shadow-sm hover-shadow transition rounded text-center h-100"></div>
+            </a>
         </div>
     </div>
 </div>
