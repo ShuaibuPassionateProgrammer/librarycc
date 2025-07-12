96e33ef06b7ef07b125549ab8641a67ab7f8a3c8
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-07-12 23:31:34 +0100

    Improving Home

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 <div class="container d-flex align-items-center justify-content-center" style="min-height: 80vh;">
     <div class="row g-5 justify-content-center">
         <!-- Member Card -->
-        <div class="col-md-4 col-sm-6"></div>
+        <div class="col-md-4 col-sm-6">
+            <a href="member/" class="text-decoration-none"></a>
+        </div>
     </div>
 </div>
