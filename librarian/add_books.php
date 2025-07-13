091c1f08d326aa3cb83ebf5f945bf4c2844f2e87
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-07-14 00:11:03 +0100

    Improvement

--- librarian/add_books.php
+++ librarian/add_books.php
@@ -29,41 +29,120 @@
     </div>
 </div>
 
-<div class="container p-2">
-    <div class="row justify-content-center">
-        <div class="col-md-12">
-            <div class="card">
-                <div class="card-body text-center">
-                    <h3>Cataloguing Work Sheet</h3>
+<div class="container py-5">
+    <div class="row justify-content-center mb-4">
+        <div class="col-lg-8 col-12">
+            <div class="text-center mb-4">
+                <h2 class="fw-bold mb-2">Cataloguing Worksheet</h2>
+                <p class="lead text-secondary">Add new books to the library collection. Please fill in all required fields.</p>
+            </div>
+            <div class="card shadow border-0">
+                <div class="card-body">
+                    <?php include("message.php"); ?>
+                    <form action="add_bookscode.php" method="post" enctype="multipart/form-data" class="row g-3">
+                        <div class="col-md-6">
+                            <label for="main_author" class="form-label">Name of Main Author</label>
+                            <input type="text" name="main_author" id="main_author" class="form-control" placeholder="Main Author" required>
+                        </div>
+                        <div class="col-md-6">
+                            <label for="editors" class="form-label">Editor(s) if any</label>
+                            <input type="text" name="editors" id="editors" class="form-control" placeholder="Editors">
+                        </div>
+                        <div class="col-md-6">
+                            <label for="edition" class="form-label">Edition</label>
+                            <input type="text" name="edition" id="edition" class="form-control" placeholder="Edition">
+                        </div>
+                        <div class="col-md-6">
+                            <label for="publication" class="form-label">Place of Publication</label>
+                            <input type="text" name="publication" id="publication" class="form-control" placeholder="Place of Publication">
+                        </div>
+                        <div class="col-md-6">
+                            <label for="pages" class="form-label">No. of Preliminary Pages</label>
+                            <input type="text" name="pages" id="pages" class="form-control" placeholder="No. of Preliminary Pages">
+                        </div>
+                        <div class="col-md-6">
+                            <label for="illustration" class="form-label">Illustration</label>
+                            <input type="text" name="illustration" id="illustration" class="form-control" placeholder="Illustration">
+                        </div>
+                        <div class="col-md-6">
+                            <label for="note" class="form-label">Note</label>
+                            <input type="text" name="note" id="note" class="form-control" placeholder="Note">
+                        </div>
+                        <div class="col-md-6">
+                            <label for="isbn" class="form-label">ISBN/ISSN</label>
+                            <input type="text" name="isbn" id="isbn" class="form-control" placeholder="ISBN/ISSN">
+                        </div>
+                        <div class="col-md-6">
+                            <label for="price" class="form-label">Price</label>
+                            <input type="text" name="price" id="price" class="form-control" placeholder="Price">
+                        </div>
+                        <div class="col-md-6">
+                            <label for="accessno" class="form-label">Access No.</label>
+                            <input type="text" name="accessno" id="accessno" class="form-control" placeholder="Access No.">
+                        </div>
+                        <div class="col-md-6">
+                            <label for="cataloger" class="form-label">Catalogued by</label>
+                            <input type="text" name="cataloger" id="cataloger" class="form-control" placeholder="Catalogued by">
+                        </div>
+                        <div class="col-md-6">
+                            <label for="book_name" class="form-label">Book Name</label>
+                            <input type="file" name="pdf_file" id="book_name" accept=".pdf" class="form-control" placeholder="Book Name">
+                        </div>
+                        <div class="col-md-6">
+                            <label for="other_author" class="form-label">Other Authors</label>
+                            <input type="text" name="other_author" id="other_author" class="form-control" placeholder="Other Authors">
+                        </div>
+                        <div class="col-md-6">
+                            <label for="title" class="form-label">Title</label>
+                            <input type="text" name="title" id="title" class="form-control" placeholder="Title">
+                        </div>
+                        <div class="col-md-6">
+                            <label for="tmaterial" class="form-label">Type of Material</label>
+                            <input type="text" name="tmaterial" id="tmaterial" class="form-control" placeholder="Type of Material">
+                        </div>
+                        <div class="col-md-6">
+                            <label for="publisher" class="form-label">Publisher</label>
+                            <input type="text" name="publisher" id="publisher" class="form-control" placeholder="Publisher">
+                        </div>
+                        <div class="col-md-6">
+                            <label for="mainpages" class="form-label">No. of Main Pages:</label>
+                            <input type="text" name="mainpages" id="mainpages" class="form-control" placeholder="No. of Main Pages:">
+                        </div>
+                        <div class="col-md-6">
+                            <label for="length" class="form-label">Length</label>
+                            <input type="text" name="length" id="length" class="form-control" placeholder="Length">
+                        </div>
+                        <div class="col-md-6">
+                            <label for="series" class="form-label">Series</label>
+                            <input type="text" name="series" id="series" class="form-control" placeholder="Series">
+                        </div>
+                        <div class="col-md-6">
+                            <label for="hbk" class="form-label">Hbk/Pbk</label>
+                            <input type="text" name="hbk" id="hbk" class="form-control" placeholder="Hbk/Pbk">
+                        </div>
+                        <div class="col-md-6">
+                            <label for="callno" class="form-label">Call No</label>
+                            <input type="text" name="callno" id="callno" class="form-control" placeholder="Call No">
+                        </div>
+                        <div class="col-md-6">
+                            <label for="subject_entry" class="form-label">Subject Added Entries</label>
+                            <input type="text" name="subject_entry" id="subject_entry" class="form-control" placeholder="Subject Added Entries">
+                        </div>
+                        <div class="col-md-6">
+                            <label for="class_cat" class="form-label">Class Category</label>
+                            <input type="text" name="class_cat" id="class_cat" class="form-control" placeholder="Class Category">
+                        </div>
+                        <div class="col-12 text-center">
+                            <a href="librarian_dashboard.php" class="btn btn-outline-secondary">Back</a>
+                            <input type="submit" name="add_book" value="Save Book" class="btn btn-outline-primary">
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
     </div>
 </div>
 
-<div class="container p-2">
-    <div class="row justify-content-center">
-        <div class="col-md-12">
-            <form action="add_bookscode.php" method="post" enctype="multipart/form-data">
-                <fieldset class="border p-2">
-                    <legend class="float-none w-auto">Book Cataloguing</legend>
-                    <div class="row">
-                        <div class="col-md-6">
-                            <table class="table table-borderless">
-                                <tbody>
-                                    <tr>
-                                        <td>Name of Main Author</td>
-                                        <td><input type="text" name="main_author" class="form-control"></td>
-                                    </tr>
-                                    <tr>
-                                        <td>Editor(s) if any</td>
-                                        <td><input type="text" name="editors" class="form-control"></td>
-                                    </tr>
-                                    <tr>
-                                        <td>Edition</td>
-                                        <td><input type="text" name="edition" class="form-control"></td>
-                                    </tr>
-                                    <tr>
                                         <td>Place of Publication</td>
                                         <td><input type="text" name="publication" class="form-control"></td>
                                     </tr>
